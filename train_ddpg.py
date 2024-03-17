04828009ecf51f8733b036d8774d3def279909d5
roxane <chen.roxane19@gmail.com>
2024-03-17 21:57:34 +0100

    f

--- train_ddpg.py
+++ train_ddpg.py
@@ -54,7 +54,7 @@ eval_env = Monitor( env)
 
 eval_callback = EvalCallback(eval_env, best_model_save_path=folder_path+"/best_model/",
                              log_path=folder_path+"/eval_log/", eval_freq=500,
-                             deterministic=True, render=False)
+                             deterministic=True, render=True)
 
 n_actions = env.action_space.shape[-1]
 
